4d7ddb6a1bf34b0800551da4b1f931cc371eb48c
yashagarwal2905 <144434628+yashagarwal2905@users.noreply.github.com>
2023-09-20 12:20:30 +0530

    Add files via upload

---
+++ Day 3/EntityModelClasses.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication3.Models
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+         : base(options)
+        {
+
+        }
+    }
+}
+
